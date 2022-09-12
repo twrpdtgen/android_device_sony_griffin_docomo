3a191b66b7d1a156d404a7aa38f751cb36aff5fd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 09:56:17 +0200

    griffin_docomo: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: sony Model: Griffin_docomo Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_griffin_docomo.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_griffin_docomo-user \
+    omni_griffin_docomo-userdebug \
+    omni_griffin_docomo-eng
